b4fafee77ee83a8cf645ce61476bb3088f571afa
Sebastiano La Monaca <sebastiano_lamonaca@yahoo.com>
2021-10-12 19:52:25 +0200

    esercizio compleatato

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php $stringa = '<h1>'.'Tu non sai le colline'.'</h1>'.'<br>'.
+'Tu non sai le colline'.'</br>'.
+'dove si è sparso il sangue'.'</br>'.
+'Tutti quanti fuggimmo'.'</br>'.
+'tutti quanti gettammo'.'</br>'.
+'l’arma e il nome. Una donna'.'</br>'.
+'ci guardava fuggire.'.'</br>'.
+'Uno solo di noi'.'</br>'.
+'si fermò a pugno chiuso,'.'</br>'.
+'vide il cielo vuoto,'.'</br>'.
+'chinò il capo e morì'.'</br>'.
+'sotto il muro, tacendo.'.'</br>'.
+'Ora è un cencio di sangue'.'</br>'.
+'il suo nome. Una donna'.'</br>'.
+'ci aspetta alle colline.'.'</br>'.
+'-Cesare Pavese'.'<hr>';
+echo $stringa;
+$stringa=str_replace("colline", "***", $stringa);
+echo $stringa;
+?>
