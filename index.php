d0bd3d43528bbb6b06cdc578a0f2df064b36546c
Sebastiano La Monaca <sebastiano_lamonaca@yahoo.com>
2021-10-13 10:09:55 +0200

    fatto con tag pre

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php $stringa = '<h1>'.'Tu non sai le colline'.'</h1>'.'<br>'.
-'Tu non sai le colline'.'</br>'.
-'dove si è sparso il sangue'.'</br>'.
-'Tutti quanti fuggimmo'.'</br>'.
-'tutti quanti gettammo'.'</br>'.
-'l’arma e il nome. Una donna'.'</br>'.
-'ci guardava fuggire.'.'</br>'.
-'Uno solo di noi'.'</br>'.
-'si fermò a pugno chiuso,'.'</br>'.
-'vide il cielo vuoto,'.'</br>'.
-'chinò il capo e morì'.'</br>'.
-'sotto il muro, tacendo.'.'</br>'.
-'Ora è un cencio di sangue'.'</br>'.
-'il suo nome. Una donna'.'</br>'.
-'ci aspetta alle colline.'.'</br>'.
-'-Cesare Pavese'.'<hr>';
+<?php $stringa = '<h1>'.'Tu non sai le colline'.'</h1>'.
+'<pre>'.'Tu non sai le colline
+dove si è sparso il sangue
+Tutti quanti fuggimmo
+tutti quanti gettammo
+l’arma e il nome. Una donna
+ci guardava fuggire
+Uno solo di noi
+si fermò a pugno chiuso
+vide il cielo vuoto,
+chinò il capo e morì
+sotto il muro, tacendo.
+Ora è un cencio di sangue
+il suo nome. Una donna
+ci aspetta alle colline'.'</pre>'.
+'<p>'.'-Cesare Pavese'.'</p>';
 echo $stringa;
 $stringa=str_replace($_GET["badword"], "***", $stringa);
 echo $stringa;
